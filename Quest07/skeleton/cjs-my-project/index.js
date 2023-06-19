2b4c75b758509c64b4a40b2d9e3b1540cd2216b7
kwonseohyung <kwonsh001@bluedigm.com>
2023-06-19 13:19:28 +0900

    Quest09 작성

--- Quest07/skeleton/cjs-my-project/index.js
+++ Quest07/skeleton/cjs-my-project/index.js
@@ -1,10 +1,13 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
 
-const {CjsUtilClass, cjsUtilFunction}= require('../cjs-package/index.js')
+const { CjsUtilClass, cjsUtilFunction } = require("../cjs-package/index.js");
+const { EsmUtilClass, esmUtilFunction } = require("../esm-package/index.mjs");
 
-//const { EsmUtilClass,esmUtilFunction}= require('../esm-package/index.mjs')
-let a=new CjsUtilClass(100)
-console.log(a.double())
-console.log(cjsUtilFunction("zzz"))
+let a = new CjsUtilClass(100);
+console.log(a.double());
+console.log(cjsUtilFunction("zzz"));
 
-//const{ EsmUtilClass,esmUtilFunction}=require('../esm-package/index.mjs');
+//에러
+let b = new EsmUtilClass(200);
+console.log(b.double());
+console.log(esmUtilFunction("hhh"));
